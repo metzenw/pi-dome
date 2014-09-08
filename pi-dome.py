30f6e8bed23f0e0b867c0b62986d73596a72bafa
Jason <metzenw@gmail.com>
2014-09-08 17:52:05 +0000

    Auth to other resources

--- pi-dome.py
+++ pi-dome.py
@@ -163,6 +163,7 @@ pi_servers = [
 
 list_of_keys = []
 list_of_server_keys = []
+MASTER_KEY = "1234"
 
 # ====================================================================
 # IP logging
@@ -180,7 +181,7 @@ def get_my_ip():
 def get_doors():
     if 'key' in request.args:
         #print(request.args['key'])
-        if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys):
+        if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
             return jsonify( { 'doors': doors } )
     return jsonify( { 'error': 'Your key was not authorized' } ), 401
 
@@ -192,7 +193,7 @@ def get_door_id(door_id):
         abort(404)
     if 'key' in request.args:
         #print(request.args['key'])
-        if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys):
+        if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
             return jsonify( { 'door': d_id[0] } )
     return jsonify( { 'error': 'Your key was not authorized' } ), 401
 
@@ -212,7 +213,7 @@ def create_door():
         'active': False,
         'open': False
     }
-    if (door['key'] in list_of_keys) or (door['key'] in list_of_server_keys):
+    if (door['key'] in list_of_keys) or (door['key'] in list_of_server_keys) or (request.args['key'] == MASTER_KEY):
         doors.append(door)
         list_of_keys.append(door['key'])
         return jsonify( { 'door': door } ), 201
@@ -229,7 +230,7 @@ def update_door(door_id):
         abort(400)
     if 'open' in request.json and type(request.json['open']) is not bool:
         abort(400)
-    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys):
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
         #door[0]['id'] = request.json.get('id', door[0]['id'])
         door[0]['key'] = request.json.get('key', door[0]['key'])
         door[0]['type'] = request.json.get('type', door[0]['type'])
@@ -248,7 +249,7 @@ def delete_door(door_id):
     door = filter(lambda t: t['id'] == door_id, doors)
     if len(door) == 0:
         abort(404)
-    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys):
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
         doors.remove(door[0])
         return jsonify( { 'result': True } )
     else:
@@ -260,7 +261,10 @@ def delete_door(door_id):
 @app.route('/api/windows/', methods = ['GET'])
 @auth.login_required
 def get_windows():
-    return jsonify( { 'windows': windows } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        return jsonify( { 'windows': windows } )
+    else:
+        return jsonify( { 'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/windows/<int:window_id>', methods = ['GET'])
 @auth.login_required
@@ -268,7 +272,10 @@ def get_window_id(window_id):
     w_id = filter(lambda t: t['id'] == window_id, windows)
     if len(w_id) == 0:
         abort(404)
-    return jsonify( { 'window': w_id[0] } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        return jsonify( { 'window': w_id[0] } )
+    else:
+        return jsonify( { 'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/windows/', methods = ['POST'])
 @auth.login_required
@@ -285,12 +292,12 @@ def create_window():
         'description': request.json.get('description', ""),
         'open': False
     }
-    if window['key'] in list_of_keys:
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
         windows.append(window)
         list_of_keys.append(window['key'])
         return jsonify( { 'window': window } ), 201
     else:
-        return jsonify( {'error': 'This node is not registered to add resources.' } ), 404
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/windows/<int:window_id>', methods = ['PUT'])
 @auth.login_required
@@ -302,15 +309,18 @@ def update_window(window_id):
         abort(400)
     if 'open' in request.json and type(request.json['open']) is not bool:
         abort(400)
-    #window[0]['id'] = request.json.get('id', window[0]['id'])
-    window[0]['key'] = request.json.get('key', window[0]['key'])
-    window[0]['type'] = request.json.get('type', window[0]['type'])
-    window[0]['gpio'] = request.json.get('gpio', window[0]['gpio'])
-    window[0]['voltage'] = request.json.get('voltage', window[0]['voltage'])
-    window[0]['notes'] = request.json.get('notes', window[0]['notes'])
-    window[0]['description'] = request.json.get('description', window[0]['description'])
-    window[0]['open'] = request.json.get('open', window[0]['open'])
-    return jsonify( { 'window': window[0] } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        #window[0]['id'] = request.json.get('id', window[0]['id'])
+        window[0]['key'] = request.json.get('key', window[0]['key'])
+        window[0]['type'] = request.json.get('type', window[0]['type'])
+        window[0]['gpio'] = request.json.get('gpio', window[0]['gpio'])
+        window[0]['voltage'] = request.json.get('voltage', window[0]['voltage'])
+        window[0]['notes'] = request.json.get('notes', window[0]['notes'])
+        window[0]['description'] = request.json.get('description', window[0]['description'])
+        window[0]['open'] = request.json.get('open', window[0]['open'])
+        return jsonify( { 'window': window[0] } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/windows/<int:window_id>', methods = ['DELETE'])
 @auth.login_required
@@ -318,8 +328,11 @@ def delete_window(window_id):
     window = filter(lambda t: t['id'] == window_id, windows)
     if len(window) == 0:
         abort(404)
-    windows.remove(window[0])
-    return jsonify( { 'result': True } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        windows.remove(window[0])
+        return jsonify( { 'result': True } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 
 # ====================================================================
@@ -328,7 +341,10 @@ def delete_window(window_id):
 @app.route('/api/garage/', methods = ['GET'])
 @auth.login_required
 def get_garage():
-    return jsonify( { 'garage': garage } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        return jsonify( { 'garage': garage } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/garage/<int:garage_id>', methods = ['GET'])
 @auth.login_required
@@ -336,7 +352,10 @@ def get_garage_id(garage_id):
     g_id = filter(lambda t: t['id'] == garage_id, garage)
     if len(g_id) == 0:
         abort(404)
-    return jsonify( { 'garage': g_id[0] } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        return jsonify( { 'garage': g_id[0] } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/garage/', methods = ['POST'])
 @auth.login_required
@@ -355,12 +374,12 @@ def create_garage():
         'open': False
     }
 
-    if g_garage['key'] in list_of_keys:
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
         garage.append(g_garage)
         list_of_keys.append(g_garage['key'])
         return jsonify( { 'garage': g_garage } ), 201
     else:
-        return jsonify( {'error': 'This node is not registered to add resources.' } ), 404
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/garage/<int:garage_id>', methods = ['PUT'])
 @auth.login_required
@@ -372,15 +391,18 @@ def update_garage(garage_id):
         abort(400)
     if 'open' in request.json and type(request.json['open']) is not bool:
         abort(400)
-    #g_garage[0]['id'] = request.json.get('id', g_garage[0]['id'])
-    g_garage[0]['key'] = request.json.get('key', g_garage[0]['key'])
-    g_garage[0]['type'] = request.json.get('type', g_garage[0]['type'])
-    g_garage[0]['gpio'] = request.json.get('gpio', g_garage[0]['gpio'])
-    g_garage[0]['voltage'] = request.json.get('voltage', g_garage[0]['voltage'])
-    g_garage[0]['notes'] = request.json.get('notes', g_garage[0]['notes'])
-    g_garage[0]['description'] = request.json.get('description', g_garage[0]['description'])
-    g_garage[0]['open'] = request.json.get('open', g_garage[0]['open'])
-    return jsonify( { 'garage': g_garage[0] } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        #g_garage[0]['id'] = request.json.get('id', g_garage[0]['id'])
+        g_garage[0]['key'] = request.json.get('key', g_garage[0]['key'])
+        g_garage[0]['type'] = request.json.get('type', g_garage[0]['type'])
+        g_garage[0]['gpio'] = request.json.get('gpio', g_garage[0]['gpio'])
+        g_garage[0]['voltage'] = request.json.get('voltage', g_garage[0]['voltage'])
+        g_garage[0]['notes'] = request.json.get('notes', g_garage[0]['notes'])
+        g_garage[0]['description'] = request.json.get('description', g_garage[0]['description'])
+        g_garage[0]['open'] = request.json.get('open', g_garage[0]['open'])
+        return jsonify( { 'garage': g_garage[0] } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/garage/<int:garage_id>', methods = ['DELETE'])
 @auth.login_required
@@ -388,9 +410,11 @@ def delete_garage(garage_id):
     g_garage = filter(lambda t: t['id'] == garage_id, garage)
     if len(g_garage) == 0:
         abort(404)
-    garage.remove(g_garage[0])
-    return jsonify( { 'result': True } )
-
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        garage.remove(g_garage[0])
+        return jsonify( { 'result': True } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 # ====================================================================
 # Temps
@@ -398,7 +422,10 @@ def delete_garage(garage_id):
 @app.route('/api/temps/', methods = ['GET'])
 @auth.login_required
 def get_temps():
-    return jsonify( { 'temps': temps } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        return jsonify( { 'temps': temps } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/temps/<int:temp_id>', methods = ['GET'])
 @auth.login_required
@@ -406,7 +433,10 @@ def get_temp_id(temp_id):
     t_id = filter(lambda t: t['id'] == temp_id, temps)
     if len(t_id) == 0:
         abort(404)
-    return jsonify( { 'temp': t_id[0] } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        return jsonify( { 'temp': t_id[0] } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/temps/', methods = ['POST'])
 @auth.login_required
@@ -425,12 +455,12 @@ def create_temp():
         'active': False
     }
 
-    if temp['key'] in list_of_keys:
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
         temps.append(temp)
         list_of_keys.append(temp['key'])
         return jsonify( { 'temp': temp } ), 201
     else:
-        return jsonify( {'error': 'This node is not registered to add resources.' } ), 404
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 #    temps.append(temp)
 #    return jsonify( { 'temp': temp } ), 201
@@ -445,16 +475,19 @@ def update_temp(temp_id_put):
         abort(400)
     if 'open' in request.json and type(request.json['open']) is not bool:
         abort(400)
-    #temp[0]['id'] = request.json.get('id', temp[0]['id'])
-    temp[0]['key'] = request.json.get('key', temp[0]['key'])
-    temp[0]['type'] = request.json.get('type', temp[0]['type'])
-    temp[0]['gpio'] = request.json.get('gpio', temp[0]['gpio'])
-    temp[0]['voltage'] = request.json.get('voltage', temp[0]['voltage'])
-    temp[0]['notes'] = request.json.get('notes', temp[0]['notes'])
-    temp[0]['temp'] = request.json.get('temp', temp[0]['temp'])
-    temp[0]['description'] = request.json.get('description', temp[0]['description'])
-    temp[0]['active'] = request.json.get('active', temp[0]['active'])
-    return jsonify( { 'temp': temp[0] } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        #temp[0]['id'] = request.json.get('id', temp[0]['id'])
+        temp[0]['key'] = request.json.get('key', temp[0]['key'])
+        temp[0]['type'] = request.json.get('type', temp[0]['type'])
+        temp[0]['gpio'] = request.json.get('gpio', temp[0]['gpio'])
+        temp[0]['voltage'] = request.json.get('voltage', temp[0]['voltage'])
+        temp[0]['notes'] = request.json.get('notes', temp[0]['notes'])
+        temp[0]['temp'] = request.json.get('temp', temp[0]['temp'])
+        temp[0]['description'] = request.json.get('description', temp[0]['description'])
+        temp[0]['active'] = request.json.get('active', temp[0]['active'])
+        return jsonify( { 'temp': temp[0] } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/temps/<int:temp_id_del>', methods = ['DELETE'])
 @auth.login_required
@@ -462,8 +495,11 @@ def delete_temp(temp_id):
     temp = filter(lambda t: t['id'] == temp_id_del, temps)
     if len(temp) == 0:
         abort(404)
-    temps.remove(temp[0])
-    return jsonify( { 'result': True } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        temps.remove(temp[0])
+        return jsonify( { 'result': True } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 # ====================================================================
 # Motion Sensors
@@ -471,7 +507,10 @@ def delete_temp(temp_id):
 @app.route('/api/motions/', methods = ['GET'])
 @auth.login_required
 def get_motions():
-    return jsonify( { 'motions': motion_sensors } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        return jsonify( { 'motions': motion_sensors } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/motions/<int:motion_id>', methods = ['GET'])
 @auth.login_required
@@ -479,7 +518,10 @@ def get_motion_id(motion_id):
     motion = filter(lambda t: t['id'] == motion_id, motion_sensors)
     if len(motion) == 0:
         abort(404)
-    return jsonify( { 'motion': motion[0] } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        return jsonify( { 'motion': motion[0] } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/motions/', methods = ['POST'])
 @auth.login_required
@@ -496,12 +538,12 @@ def create_motion():
         'description': request.json.get('description', ""),
         'active': False
     }
-    if motion['key'] in list_of_keys:
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
         motion_sensors.append(motion)
         list_of_keys.append(motion['key'])
         return jsonify( { 'motion': motion } ), 201
     else:
-        return jsonify( {'error': 'This node is not registered to add resources.' } ), 404
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/motions/<int:motion_id>', methods = ['PUT'])
 @auth.login_required
@@ -513,15 +555,18 @@ def update_motion(motion_id):
         abort(400)
     if 'open' in request.json and type(request.json['open']) is not bool:
         abort(400)
-    #motion[0]['id'] = request.json.get('id', motion[0]['id'])
-    motion[0]['key'] = request.json.get('key', motion[0]['key'])
-    motion[0]['type'] = request.json.get('type', motion[0]['type'])
-    motion[0]['gpio'] = request.json.get('gpio', motion[0]['gpio'])
-    motion[0]['voltage'] = request.json.get('voltage', motion[0]['voltage'])
-    motion[0]['notes'] = request.json.get('notes', motion[0]['notes'])
-    motion[0]['description'] = request.json.get('description', motion[0]['description'])
-    motion[0]['active'] = request.json.get('active', motion[0]['active'])
-    return jsonify( { 'motion': motion[0] } )
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        #motion[0]['id'] = request.json.get('id', motion[0]['id'])
+        motion[0]['key'] = request.json.get('key', motion[0]['key'])
+        motion[0]['type'] = request.json.get('type', motion[0]['type'])
+        motion[0]['gpio'] = request.json.get('gpio', motion[0]['gpio'])
+        motion[0]['voltage'] = request.json.get('voltage', motion[0]['voltage'])
+        motion[0]['notes'] = request.json.get('notes', motion[0]['notes'])
+        motion[0]['description'] = request.json.get('description', motion[0]['description'])
+        motion[0]['active'] = request.json.get('active', motion[0]['active'])
+        return jsonify( { 'motion': motion[0] } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 @app.route('/api/motions/<int:motion_id>', methods = ['DELETE'])
 @auth.login_required
@@ -529,9 +574,11 @@ def delete_motion(motion_id):
     motion = filter(lambda t: t['id'] == motion_id, motion_sensors)
     if len(motion) == 0:
         abort(404)
-    motion_sensors.remove(motion[0])
-    return jsonify( { 'result': True } )
-
+    if (request.args['key'] in list_of_server_keys) or (request.args['key'] in list_of_keys) or (request.args['key'] == MASTER_KEY):
+        motion_sensors.remove(motion[0])
+        return jsonify( { 'result': True } )
+    else:
+        return jsonify( {'error': 'Your key was not authorized' } ), 401
 
 # ====================================================================
 # pi_nodes
