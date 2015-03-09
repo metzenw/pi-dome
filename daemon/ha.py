923b5ae89ce7a88a66b4b162677d2047ff43c6a3
Jason <metzenw@gmail.com>
2015-03-09 17:24:58 +0000

    Test of mag sensor and daemon integration

--- daemon/ha.py
+++ daemon/ha.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import sys, time, signal
+import sys, time, signal 
+import RPi.GPIO as GPIO
 from daemon import Daemon
 
 #s_log = open("/tmp/home_automation.log", "a")
@@ -8,24 +9,86 @@ from daemon import Daemon
 
 class MyDaemon(Daemon):
    def run(self):
+      GPIO.setmode(GPIO.BOARD)
+      gpio11 = 12 #Garage Door Down
+      gpio13 = 16 #Garage Door up
+      gpio16 = 18 #Door Kitchen
+      gpio18 = 15 #Door Porch
+
+      #GPIO.setup(gpio12, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+      GPIO.setup(gpio11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+      GPIO.setup(gpio13, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+      GPIO.setup(gpio16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+      GPIO.setup(gpio18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+      def printMag(channel):
+         self.logger.write("INFO:   Mag intact:   " + channel)
+
+      def write_logs(my_str):
+         file = open("/var/www/door.log", "w+", 0)
+         file.write(my_str)
+         file.close()
+
+      gd_open_state = 1 
+      gd_closed_state = 1
+
+      g_to_porch_state = 1
+      g_to_kitchen_state = 1
+
+      gd_open_string = "Garage Door is <b>Open</b>. </br>"
+      gd_closed_string = "Garage Door is <b>Closed</b>. </br>"
+      gd_moving_string = "Garage Door <b>moving</b>. </br>"
+
+      door_to_porch_closed_string = "Door from garage to porch is <b>Closed</b>. </br>"
+      door_to_porch_open_string = "Door  from garage to porch is <b>open</b>. </br>"
+      door_from_garage_to_kitcken_open_str = "Door from kitchen to garage is <b>open</b>. </br>"
+      door_from_garage_to_kitchen_closed_str = "Door from kitchen to garage is <b>Closed</b>. </br>"
+
+      #While loop
       while True:
-         time.sleep(1)
-         self.logger.write("INFO:   Daemon running....\n")
+         time.sleep(0.5)
+         #self.logger.write("INFO:   Daemon running....\n")
+         str_to_write = ""
+         write_out_logs = 0
 
-#Function to handel signals
-#def signalHandler(s_signal,b):
-   #print "Signal received ",s_signal,"==",b;
-#   tmp_signal_log = "Signal received " + str(s_signal)
-#   s_log.write(tmp_signal_log)
-#   if s_signal == 15:
-#      daemon.stop()
-#   if s_signal == 2:
-#      daemon.stop()
+         if (GPIO.input(gpio11) == 0 and GPIO.input(gpio13) == 0):
+            #print("GarageDoor moving.")
+            str_to_write = str_to_write + gd_moving_string
+         elif (GPIO.input(gpio11) == 1):
+            #print("GarageDoor is Closed.")
+            str_to_write = str_to_write + gd_closed_string
+         elif (GPIO.input(gpio13) == 1):
+            #print("GarageDoor is Open.")
+            str_to_write = str_to_write + gd_open_string
+         if (GPIO.input(gpio16) == 1):
+            #print("Door from kitchen to garage is Closed.")
+            str_to_write = str_to_write + door_from_garage_to_kitchen_closed_str
+         else:
+            #print("Door from kitchen to garage is open.")
+            str_to_write = str_to_write + door_from_garage_to_kitcken_open_str
+         if (GPIO.input(gpio18) == 1):
+            #print("Door from garage to porch is Closed.")
+            str_to_write = str_to_write + door_to_porch_closed_string
+         else:
+            #print("Door  from garage to porch is open.")
+            str_to_write = str_to_write + door_to_porch_open_string 
+         #States and logs
+         if (GPIO.input(gpio11) != gd_closed_state ):
+            write_out_logs = 1
+            gd_closed_state = GPIO.input(gpio11)
+         elif ( GPIO.input(gpio13) != gd_open_state ):
+            write_out_logs = 1 
+            gd_open_state = GPIO.input(gpio13)
+         elif ( GPIO.input(gpio16) != g_to_kitchen_state ):
+            write_out_logs = 1
+            g_to_kitchen_state = GPIO.input(gpio16)
+         elif ( GPIO.input(gpio18) != g_to_porch_state ):
+            write_out_logs = 1
+            g_to_porch_state = GPIO.input(gpio18)
+		
 
-#Define how to handel signals
-#signal.signal(signal.SIGINT,signalHandler);
-#signal.signal(signal.SIGUSR1,signalHandler);
-#signal.signal(signal.SIGTERM,signalHandler);
+         if (write_out_logs == 1):
+            #print("Writing to logs.")
+            write_logs(str_to_write)
 
 if __name__ == "__main__":
    daemon = MyDaemon('/var/run/home_automation.pid')
