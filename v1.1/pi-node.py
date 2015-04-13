38312eff4c5aa8b87827cd660720b728381f8855
Jason <jbooth@localhost.localdomain>
2015-04-12 21:24:31 -0600

    Added init for the node

--- v1.1/pi-node.py
+++ v1.1/pi-node.py
@@ -1,2 +1,15 @@
-#!/usr/bin/python
-from include.PIgeneral import *
+#!/usr/bin/env python
+from lib.PInode import *
+import lib.PIgeneral as PIgeneral
+
+
+# ====================================================================
+# Main entry into the pi-server
+# ====================================================================
+def main():
+    pi_server = PInode()
+    pi_server.init()
+
+if __name__ == '__main__':
+    main()
+
