3c5eddb78c5b4944053500ceb2477ecd78a0a598
Jason <metzenw@gmail.com>
2015-04-16 15:22:25 -0600

    pi node start script fixes

--- v1.1/pi-node.py
+++ v1.1/pi-node.py
@@ -8,16 +8,21 @@ import lib.PIgeneral as PIgeneral
 config = ConfigParser.RawConfigParser()
 config.read('node.cfg')
 model = config.get('node', 'model')
+server_ipaddr = config.get('server', 'ipaddr')
 
 # ====================================================================
-# Main entry into the pi-server
+# Main entry into the pi-node
 # ====================================================================
 def main():
     pi_node = PInode()
-    pi_node.init("node", model) #Supported b, b+, b+2, and c1
+    pi_node.init("node", model, config) #Supported b, b+, b+2, and c1
     while 1:
         pi_node.monitor_gpio()
         time.sleep(0.5)
+
+# ====================================================================
+# Application start
+# ====================================================================
 if __name__ == '__main__':
     main()
 
