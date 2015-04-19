7d3c4d55c3a5ed2c670a37878c5389bfb584be76
Jason <metzenw@gmail.com>
2015-04-18 18:54:02 -0600

    Server functions

--- v1.1/lib/PIconnection.py
+++ v1.1/lib/PIconnection.py
@@ -4,17 +4,12 @@
 from socket import *
 from ssl import *
 
-#user is not finnished
-finnished =False
-
-#create socket
-client_socket=socket(AF_INET, SOCK_STREAM)
-tls_client = wrap_socket(client_socket, ssl_version=PROTOCOL_TLSv1, cert_reqs=CERT_NONE)
 
 class PIconnection:
   #'Common base class for PIconnection'#
    def __init__(self, client_or_server, server_name, server_port):
       self.client_socket = socket(AF_INET, SOCK_STREAM)
+      self.server_socket = socket(AF_INET, SOCK_STREAM)
       self.tls_client_socket = wrap_socket(self.client_socket, ssl_version=PROTOCOL_TLSv1, cert_reqs=CERT_NONE)
       self.server_name = server_name
       self.server_port = server_port
@@ -48,10 +43,53 @@ class PIconnection:
    # ====================================================================
    #  Client disconnect - disconnect from server
    # ====================================================================
-   def client_disconnect(self):
+      def client_disconnect(self):
       #close the socket
       self.client_socket.shutdown(SHUT_RDWR)
       self.client_socket.close()
+
+
+   # ====================================================================
+   #  Serve bind - server bind
+   # ====================================================================
+   def server_bind(self):
+      self.server_socket.bind((self.server_name, self.server_port))
+
+      #listen for connection
+      self.server_socket.listen (1)
+      self.tls_server = wrap_socket(server_socket, ssl_version=PROTOCOL_TLSv1, cert_reqs=CERT_NONE, server_side=True, keyfile='my.key', certfile='my.crt')
+
+      print('server started')
+
+   def server_listen(self):
+      #accept connection
+      self.connection, client_address= tls_server.accept()
+      print ('connection from', client_address)
+
+
+      #send and receive data from the client socket
+      data_in=connection.recv(1024)
+      message=data_in.decode()
+      print('client send',message)
+      message = "Got it!"
+      data_out=message.encode()
+      connection.send(data_out)
+
+   # ====================================================================
+   #  Server cleanup - clean up server connections.
+   # ====================================================================
+   def server_disconnect(self):
+      #close the connection
+      self.connection.shutdown(SHUT_RDWR)
+      self.connection.close()
+
+      #close the server socket
+      self.server_socket.shutdown(SHUT_RDWR)
+      self.server_socket.close()
+
+
+
+
 
    # ====================================================================
    #  Client client_update - disconnect from server
@@ -64,5 +102,6 @@ class PIconnection:
          self.client_disconnect
       if self.service_type == "server":
          print "Server connect."
-
+         self.server_bind()
+         self.server_disconnect()
 
