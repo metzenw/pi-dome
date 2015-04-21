1abc1b323992ea9103dfb8b769a9b8ec2d351b71
Jason <metzenw@gmail.com>
2015-04-21 15:59:49 -0600

    rest get script

---
+++ v1.1/rest_get.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import httplib
+import base64
+import string
+import json
+
+
+def get(base, port, url):
+	conn = httplib.HTTPConnection(base, port, timeout=60)
+	conn.request('GET', url, None, { 'Authorization' : 'Basic '+string.strip(base64.encodestring('pi-dome:pi-dome'))})
+	return conn.getresponse().read()
+
+data = get("vengersonline.com", 5000, "/api/nodes/")
+print json.loads(data)
