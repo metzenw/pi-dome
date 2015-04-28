d1bde5a5e63ecc284344104b62d351c9381d9353
Jason <metzenw@gmail.com>
2015-04-28 10:06:23 -0600

    temp lib

--- v1.1/lib/PItemp.py
+++ v1.1/lib/PItemp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import Adafruit_DHT as dht
 import time
+import calendar
 
 try:
    import Adafruit_DHT as dht
@@ -13,13 +14,24 @@ class PItemp:
    #'Common base class for PInodes'#
    def __init__(self):
       self.name = "Temp and humidity."
-
+      self.delta = 10
+      self.track_delta = calendar.timegm(time.gmtime())
+      self.reset_delta_count = calendar.timegm(time.gmtime())      
+      self.fahrenheit = 0.0
+      self.humidity = 0.0
+   # ====================================================================
+   #  Updated every 10 seconds for temp and humidity
+   # ====================================================================
    def get_temp_and_humidity(self):
       if DHT_FOUND:
-         h,t = dht.read_retry(dht.DHT11, 4)
-         #print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h)
-         fahrenheit = t*1.8000 + 32.00
-         #print "Fahrenheit: " + str(fahrenheit)
-         return fahrenheit, h
+        self.track_delta = calendar.timegm(time.gmtime())
+        if (self.track_delta - self.reset_delta_count > self.delta):
+           #Reset times
+           self.track_delta = calendar.timegm(time.gmtime())
+           self.reset_delta_count = calendar.timegm(time.gmtime())
+           #Call DHT and get temp, humidity.
+           self.humidity,t = dht.read_retry(dht.DHT11, 4)
+           self.fahrenheit = t*1.8000 + 32.00
+        return self.fahrenheit, self.humidity
 
 
--- v1.1/pi-node.py
+++ v1.1/pi-node.py
@@ -4,6 +4,7 @@ import time
 from lib.PInode import *
 import lib.PIgeneral as PIgeneral
 from lib.PIconnection import *
+from lib.PItemp import *
 
 config = ConfigParser.RawConfigParser()
 config.read('node.cfg')
@@ -15,19 +16,25 @@ server_port = int(config.get('server', 'port'))
 # Main entry into the pi-node
 # ====================================================================
 def main():
+   #Create a temperature
+   pi_temp = PItemp()
+   #Create a node
    pi_node = PInode()
    pi_node.init("node", model, config) #Supported b, b+, b+2, and c1
    pi_node.convert_gpio_to_jason()
- 
+   #Create a client connection 
    pi_client_con = PIconnection("client", server_ipaddr, server_port)
    pi_client_con.init()
 
    print("Connecting to: ", server_ipaddr, server_port)
    while 1:
+      far, hum = pi_temp.get_temp_and_humidity()
       pi_node.monitor_gpio()
       pi_node_json_gpio = pi_node.convert_gpio_to_jason()
       #print pi_node_json_gpio
-      time.sleep(1.0)
+     
+      #Add temp and far to serer update.
+      #pi_node_json_gpio['']
 
       try:
          if pi_node_json_gpio:
@@ -38,6 +45,7 @@ def main():
       except:
          print("Unable to connect to: " + pi_client_con.server_name)
 
+      time.sleep(0.2)
 
 # ====================================================================
 # Application start
